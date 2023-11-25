c3fa03da836878ca786e9df37eb0f887f86b1a7b
pankaj50000 <japankaj282@gmail.com>
2023-11-25 23:58:42 +0530

    init change

---
+++ studenttimetable/src/main/java/com/esdproj/studenttimetable/StudenttimetableApplication.java
@@ -0,0 +1,13 @@
+package com.esdproj.studenttimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenttimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenttimetableApplication.class, args);
+	}
+
+}
---
+++ studenttimetable/src/test/java/com/esdproj/studenttimetable/StudenttimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esdproj.studenttimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenttimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
